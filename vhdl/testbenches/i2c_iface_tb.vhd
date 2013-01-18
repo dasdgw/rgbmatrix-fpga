d35af24aec98a690a75ce5b861df18fa8d6ae604
frankalicious <af.frankalicious@googlemail.com>
2013-01-18 21:15:05 +0100

    add marker for testcase
    this way the testcase can better be recognized

--- vhdl/testbenches/i2c_iface_tb.vhd
+++ vhdl/testbenches/i2c_iface_tb.vhd
@@ -145,16 +145,16 @@ begin  -- architecture testbench
   begin
     report "start i2c simulation: ..." severity note;
     -- insert signal assignments here
-    report "write 0xAA to the slave address" severity note;
     i2c_write("1010101", x"AA");
+    report "TC0: write 0xAA to the slave address" severity note;
     wait for 100 us;
-    report "write 0xAA to the wrong slave address. no one should ack the address." severity note;
+    report "TC1: write 0xAA to the wrong slave address. no one should ack the address." severity note;
     i2c_write("1010000", x"AA");
     wait for 100 us;
 --    i2c_write("1010101", x"AA");
 --    wait for 100 us;
-    report "write 0xAAAAAA to the slave address" severity note;
     i2c_write("1010101", x"AAAAAA");
+    report "TC2: write 0xAAAAAA to the slave address" severity note;
     wait until clk = '1';
   end process WaveGen_Proc;
 
